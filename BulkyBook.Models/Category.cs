21be385bb729f52d7a18c22594e63738ff0a29e5
David Sadler <davidwallacesadler@gmail.com>
2020-04-19 11:06:12 -0600

    Add category model and push to the database

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        // With entity framework if the name of a property is Id will automatically be a primary key
+        // OTherwise have to explicitly annotate it with a [Key]
+        public int Id { get; set; }
+       
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
