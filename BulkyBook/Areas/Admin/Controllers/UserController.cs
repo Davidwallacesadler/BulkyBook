75be85ceb17fbb5011a08d8deea641616160b5a6
David Sadler <davidwallacesadler@gmail.com>
2020-04-23 10:44:50 -0600

    Implement UserController with basic index view and registration insert

---
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        // NOTE: we are going to use the Db context here for demo purposes 
+        //  - In a real app, only use one technology (so if using repository pattern, dont switch to db context like we did here)
+        private readonly ApplicationDbContext _db;
+
+        
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // NOTE: We are using dataTables that are loaded with an API
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach(var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if(user.Company==null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+
+            }
+            return Json(new { data = userList });
+        }
+        #endregion
+    }
+}
